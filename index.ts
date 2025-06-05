7743e7cbf3a24fd2adcaf7fc54d04eb235412466
Mohamed Doumbia <91441124+momostifler96@users.noreply.github.com>
2025-06-05 17:57:45 +0000

    update main.scss and index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import 'flatpickr/dist/flatpickr.css';
+import './css/main.css'
 import './css/main.scss'
+import 'flatpickr/dist/flatpickr.css';
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
