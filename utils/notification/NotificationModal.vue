2d700e7956ece9c9852945b7db75766d5c451d05
Mohamed Doumbia <91441124+momostifler96@users.noreply.github.com>
2025-06-07 17:18:19 +0000

    update NotificationModal.vue and notificationStore.ts

--- utils/notification/NotificationModal.vue
+++ utils/notification/NotificationModal.vue
@@ -100,12 +100,12 @@ onMounted(async () => {
                         <p class="text-sm text-gray-400">{{ notification.date }}</p>
                         <div class="flex gap-3 mt-2" v-if="notification.url && notification.item_id">
                             <button class="app-btn small success"
-                                @click="notificationStore.notifAction(true, notification.url, notification.item_id, notification.id)">
+                                @click="notificationStore.notifAction(true, notification)">
                                 <CheckIcon class="size-4" />
                                 <span>Accèpter</span>
                             </button>
                             <button class="app-btn small danger"
-                                @click="notificationStore.notifAction(false, notification.url, notification.item_id, notification.id)">
+                                @click="notificationStore.notifAction(false, notification)">
                                 <XIcon class="size-4" />
                                 <span>Refuser</span>
                             </button>
