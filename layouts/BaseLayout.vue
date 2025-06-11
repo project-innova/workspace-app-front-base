f30e8fbad76b4e9dd62193a985f93c50a4f68963
Mohamed Doumbia <91441124+momostifler96@users.noreply.github.com>
2025-06-11 11:16:03 +0000

    update BaseLayout.vue

--- layouts/BaseLayout.vue
+++ layouts/BaseLayout.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="flex w-screen h-screen gap-2 ">
-        <BaseSideBase />
-        <main
-            class=" h-full gap-2 overflow-y-auto rounded-lg w-[calc(100vw-64px)] ml-[64px] pt-2 pr-3 transition-[width,margin-left]"
-            :class="{ 'lg:w-[calc(100vw-360px)] lg:ml-[370px]': sideStore.show }">
-            <LayoutHeader :auth-user="authStore.user!" />
-            <div class="mt-3 h-[calc(100vh-90px)] overflow-y-auto">
-                <component :is="$route.meta.header" :auth-user="authStore.user!" />
-                <router-view>
-                    <template #default="{ Component, route }">
-                        <keep-alive>
-                            <component :is="Component" :key="route"></component>
-                        </keep-alive>
-                    </template>
-                </router-view>
-            </div>
-        </main>
-    </div>
+  <div class="flex w-screen h-screen gap-2 ">
+    <BaseSideBase />
+    <main
+      class=" h-full gap-2 overflow-y-auto rounded-lg w-[calc(100vw-64px)] ml-[64px] pt-2 pr-3 transition-[width,margin-left]"
+      :class="{ 'lg:w-[calc(100vw-360px)] lg:ml-[370px]': $drawerPages != '!*' && sideStore.show }">
+      <LayoutHeader :auth-user="authStore.user!" />
+      <div class="mt-3 h-[calc(100vh-90px)] overflow-y-auto">
+        <component :is="$route.meta.header" :auth-user="authStore.user!" />
+        <router-view>
+          <template #default="{ Component, route }">
+            <keep-alive>
+              <component :is="Component" :key="route"></component>
+            </keep-alive>
+          </template>
+        </router-view>
+      </div>
+    </main>
+  </div>
 </template>
 <script setup lang="ts">
 import { onMounted } from 'vue'
@@ -28,6 +28,6 @@ const authStore = useAuthStore()
 const sideStore = useSideBar()
 
 onMounted(() => {
-    authStore.loadUser()
+  authStore.loadUser()
 })
 </script>
